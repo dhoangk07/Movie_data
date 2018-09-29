383c34c57247a0b78b36c279f6344effe7e24553
HoangDinh <dhoangk07@gmail.com>
2018-09-29 16:29:40 +0700

    Add field to show image for film

---
+++ db/migrate/20180929090609_add_poster_path_and_backdrop_path_to_movies_table.rb
@@ -0,0 +1,6 @@
+class AddPosterPathAndBackdropPathToMoviesTable < ActiveRecord::Migration[5.2]
+  def change
+    add_column :movies, :poster_path, :string
+    add_column :movies, :backdrop_path, :string
+  end
+end
