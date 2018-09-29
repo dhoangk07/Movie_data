39b4aeb6cd01e1f045d3948a8cb5d9a676dbbb0e
HoangDinh <dhoangk07@gmail.com>
2018-09-29 11:12:49 +0700

    Add controller Movie

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,31 @@
+class MoviesController < ApplicationController
+  before_action :set_movie, only: %i[edit update show destroy]
+  def index
+  end
+  
+  def new
+  end
+  
+  def create
+  end 
+  
+  def edit
+  end
+  
+  def update
+  end
+  
+  def show
+  end
+  
+  def destroy
+  end
+  private
+    def set_movie
+      @movie = Movie.find(params[:id])
+    end
+    
+    def movie_params
+      params.require(:movie).permit(:title, :description, :imdb)
+    end
+end
